5e951084a31fa2d171af27d18629934d6b89f452
Zijung Chueh <i@zijung.me>
2019-07-10 15:30:49 +0800

    feature(CSDN 博客淨化): 新增

---
+++ CSDN 博客淨化.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         CSDN 博客淨化
+// @namespace    https://zijung.me/
+// @version      0.1
+// @description  取消 CSDN 博客的「展開閱讀全文」
+// @author       Zijung Chueh <i@zijung.me>
+// @license      MIT
+// @match        *://blog.csdn.net/*
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let hide_article_box = document.querySelector('.hide-article-box');
+    if (hide_article_box) {
+        hide_article_box.remove();
+    }
+
+    let article_content = document.querySelector('#article_content');
+    if (article_content) {
+        article_content.style = "";
+    }
+})();
